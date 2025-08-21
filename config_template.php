6c5a44d2f455db27f76b970b669ad08b793bcafb
Jean Roussie <jean.roussie@free.fr>
2025-08-21 11:07:03 +0200

    Create config_template.php

---
+++ config_template.php
@@ -0,0 +1,29 @@
+<?php
+	/**
+	* Configuration site
+	*
+	*/
+	
+	$site_name = "Nom du site";
+	$admin_email = "email@administrateur";
+	$site_url = "https://adresse.du.site";
+		
+	/**
+	* Configuration pour database connection
+	*
+	*/
+	
+	$host = "localhost";
+	$username = "login_base_de_donnees";
+	$password = "mdp_base_de_donnees";
+	$dbname = "nom_base_de_donnees";
+	
+	/**
+	* Super admins
+	*
+	*/
+	
+	#$admins : utilisateurs aux droits d'administration irrévocables
+	$admins = ['utilisateur_super_admin'];
+	
+?>
