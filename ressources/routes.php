d32798d162514557d7310d7bd6566d1ada2cfcf3
Jean Roussie <jean.roussie@free.fr>
2026-08-31 14:58:48 +0200

    Update routes.php
    Correction du fichier Routes pour affichage correct des pdf équipements

--- ressources/routes.php
+++ ressources/routes.php
@@ -24,9 +24,11 @@ $routes->add('forgot_password_confirm', new Route('/mot_de_passe_oublie/confirma
 $routes->add('reset_password', new Route('/regenerer_mot_de_passe', ['_controller' => 'Epiclub\\Controller\\AppUserRegisterController', 'action' => 'resetPassword']));
 
 $routes->add('equipement_list', new Route('/equipements', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'list']));
-$routes->add('equipement_list_excel', new Route('/equipements/excel-liste', ['_controller' => 'Epiclub\\Controller\\EquipementController','action' => 'listExcel']));
-$routes->add('equipement_show', new Route('/equipements/equipement-{id}', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'show']));
+$routes->add('equipement_list_excel', new Route('/equipements/excel-liste', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'listExcel']));
+$routes->add('equipement_etiquettes', new Route('/equipements/etiquettes', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'etiquettesPdf']));
+$routes->add('equipement_pdf', new Route('/equipements/equipement-pdf-{id}', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'pdf']));
 $routes->add('equipement_edit', new Route('/equipements/equipement_modification-{id}', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'edit']));
+$routes->add('equipement_show', new Route('/equipements/equipement-{id}', ['_controller' => 'Epiclub\\Controller\\EquipementController', 'action' => 'show']));
 
 // JOURNAUX - NOUVEAU
 $routes->add('journal_list', new Route('/journaux', ['_controller' => 'Epiclub\\Controller\\JournalController', 'action' => 'index']));
